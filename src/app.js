c8455fa9d36f819f8250211c7d643ee9173e9131
qashi271218 <qasimudemy@gmail.com>
2024-02-03 12:39:35 +0530

    init commit

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const hbs = require('hbs');
 const app = express();
 const port = 3000;
 
-//Define path for express config
+//Define paths for express config
 const publicDirPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
